7a7e821482bcb998847e33ceac786cc4fcbd21f5
jsz6389 <jsz6389@rit.edu>
2022-08-05 15:57:11 -0400

    Function args can now be mapped

--- mcsema/substitutions.cpp
+++ mcsema/substitutions.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <fstream>
 #include <utility>
+#include <map>
 
 #include <llvm/IR/Module.h>
 #include <llvm/IR/PassManager.h>
@@ -33,6 +34,14 @@ using llvm::Type;
 using llvm::ArrayRef;
 using llvm::ConstantDataArray;
 using namespace llvm;
+
+
+class func_map {                // A mapping of one function to another
+    public:                     //
+    const char* og_func;              // The name of the original function
+    const char* new_func;             // The name of the function that the original function will be replaced with
+    std::map<int, int> args;    // A map of the arguments by index in the original function to the new function
+};
 
 
 /* Dumps the LLVM IR from a module to a file
@@ -65,39 +74,48 @@ void dump(const char* path, std::unique_ptr<Module>& mod)
  */
 void create_declaration(const std::unique_ptr<Module>& mod, IRBuilder<>& builder, const char* func_name)
 {
-    std::vector<Type*> args = { builder.getInt8Ty()->getPointerTo(), builder.getInt64Ty() };
-    auto function_type = FunctionType::get(builder.getInt64Ty(), args, false);
+    //std::vector<Type*> args = { builder.getInt8Ty()->getPointerTo(), builder.getInt64Ty() };
+    std::vector<Type*> args = {};
+    auto function_type = FunctionType::get(builder.getInt64Ty(), args, true);
 
     mod->getOrInsertFunction(func_name, function_type);
     printf("Created declaration for external function %s\n", func_name);
 }
 
 
-/* Locates where a function is called within a module
+/* Subititutes one function for another within a module
  *
  * @param func_name The name of the function to find
  *
  * @param mod The module
  */
-void find_call(const char* og_func, const char* new_func, const std::unique_ptr<Module>& mod, IRBuilder<>& builder)
+void substitute(func_map map, const std::unique_ptr<Module>& mod, IRBuilder<>& builder)
 {
-    Function* function_call = mod->getFunction(og_func);
-    Function* new_func_inst = mod->getFunction(new_func);
+    Function* function_call = mod->getFunction(map.og_func);
+    Function* new_func_inst = mod->getFunction(map.new_func);
     
     for (const auto& user : function_call->users()){
         if (!llvm::isa<CallInst>(user)){
             continue;
         }
-        printf("Identified a call to %s\n", og_func);
-    /* TODO Substitute function call
-     * https://llvm.org/doxygen/classllvm_1_1Function.html
-     * https://llvm.org/doxygen/namespacellvm.html
-     *
-     */
+        printf("Identified a call to %s\n", map.og_func);
+        /* TODO Substitute function call
+         * https://llvm.org/doxygen/classllvm_1_1Function.html
+         * https://llvm.org/doxygen/namespacellvm.html
+         *
+         */
+        printf("%d operands in %s\n", user->getNumOperands(), map.og_func);
+
+        const int arg_size = map.args.size();
+        std::array<llvm::Value*, 7> new_func_args;
+        for (const auto &item : map.args) {
+            new_func_args[item.second] = user->getOperand(item.first);
+        }
+
         const auto call_instruction = llvm::cast<CallInst>(user);
         builder.SetInsertPoint(call_instruction->getNextNode());
         llvm::Value *hello = builder.CreateGlobalStringPtr("Hello world!\n");
-        builder.CreateCall(new_func_inst, {hello});
+        builder.CreateCall(new_func_inst, new_func_args);
     }
 }
 
@@ -119,9 +137,19 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    std::string func_name = "printf";
+    func_map myMap;
+    myMap.og_func = "__printf_chk";
+    myMap.new_func = "printf";
+    myMap.args[1] = 0;
+    myMap.args[2] = 1;
+    myMap.args[3] = 2;
+    myMap.args[4] = 3;
+    myMap.args[5] = 4;
+    myMap.args[6] = 5;
+    myMap.args[7] = 6;
+
 	create_declaration(Mod, builder, "printf");
-    find_call("__printf_chk", "printf", Mod, builder);
+    substitute(myMap, Mod, builder);
     dump("output.ll", Mod);
 
 }
